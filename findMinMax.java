12a8ae6991eb145d998c01b9d565b88171e81d86
engincanoz <ozengincan424@gmail.com>
2023-09-20 15:53:02 +0300

    Find the minimum and maximum element in the array

---
+++ findMinMax.java
@@ -0,0 +1,28 @@
+
+
+public class findMinMax {
+    public static int findMax(int[] arr) {
+        int max;
+        max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public static int findMin(int[] arr) {
+        int min;
+        min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+
+}
